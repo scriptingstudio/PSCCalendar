7067b4e0897e1426c74bd00b40765889dcdfe533
Matthew Gray <smotienko@gmail.com>
2022-03-16 16:46:12 +0300

    miscellaneous functions

--- functions.ps1
+++ functions.ps1
@@ -1,9 +1,10 @@
 # A set of miscellaneous functions
 
-function transpose-object ([string[]]$header, [switch]$include) {
+function transpose-object ([string[]]$header, [switch]$include, [string]$prefix='col') {
 # $include - includes original header in the output object as the first column
     begin {
         $table = [System.Collections.Generic.List[object]]::new()
+        if (-not $prefix) {$prefix = 'col'}
     }
     process {
         $table.add($_)
@@ -19,22 +20,22 @@ function transpose-object ([string[]]$header, [switch]$include) {
             }
             return
         }
-        $rows = $table[0].psobject.properties.name
+        $rows = $table[0].psobject.properties.name # row identities
         $colcount = $table.count
         if ($include) {$colcount++}
         $newheader = if ($header) {$header} 
-        else {for ($i=0; $i -lt $colcount; $i++) {"col$i"}}
+        else {for ($i=0; $i -lt $colcount; $i++) {"$prefix$i"}}
         $start = if ($include) {1} else {0}
+        # Build new object
+        $objwidth = $newheader.count
         foreach ($p in $rows) {
             $row = '' | Select-Object $newheader
-            if ($include) {
-                $row.($newheader[0]) = $p
-            }
+            if ($include) {$row.($newheader[0]) = $p}
             $i = $start
+            # Import data
             foreach ($v in $table.$p) {
-                if ($i -ge $newheader.count) {break}
-                $row.($newheader[$i]) = $v
-                $i++
+                if ($i -ge $objwidth) {break}
+                $row.($newheader[$i++]) = $v
             }
             $row
         }
