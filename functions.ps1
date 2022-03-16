4c65f04a21385b94e6ecb50881180017f3b947bc
Matthew Gray <smotienko@gmail.com>
2022-03-16 13:33:25 +0300

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -1,1 +1,39 @@
-
+function transpose-object ([string[]]$header, [switch]$include) {
+	begin {
+		$table = [System.Collections.Generic.List[object]]::new()
+	}
+	process {
+		$table.add($_)
+	}
+	end {
+		if ($table.count -eq 1) {
+			$newheader = if ($header -and $header.count -gt 1) {$header} else {'property','value'}
+			$_.psobject.properties.foreach{
+				[pscustomobject]@{
+					$newheader[0] = $_.name
+					$newheader[1] = $_.value
+				}
+			}
+			return
+		}
+		$rows = $table[0].psobject.properties.name
+		$colcount = $table.count
+		if ($include) {$colcount++}
+		$newheader = if ($header) {$header} 
+		else {for ($i=0; $i -lt $colcount; $i++) {"col$i"}}
+		$start = if ($include) {1} else {0}
+		foreach ($p in $rows) {
+			$row = '' | Select-Object $newheader
+			if ($include) {
+				$row.($newheader[0]) = $p
+			}
+			$i = $start
+			foreach ($v in $table.$p) {
+				if ($i -ge $newheader.count) {continue}
+				$row.($newheader[$i]) = $v
+				$i++
+			}
+			$row
+		}
+	}
+} # END transpose-object
