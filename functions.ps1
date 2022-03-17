0727f86f9f9f5a41f62185b128f7b1081719b962
Matthew Gray <smotienko@gmail.com>
2022-03-17 10:32:14 +0300

    miscellaneous functions

--- functions.ps1
+++ functions.ps1
@@ -1,60 +1,5 @@
 # A set of miscellaneous functions
 
-function transpose-object ([string[]]$header, [switch]$include, [string]$prefix='col') {
-# $include - includes original header in the output object as the first column
-    begin {
-        $table = [System.Collections.Generic.List[object]]::new()
-        if (-not $prefix) {$prefix = 'col'}
-    }
-    process {
-        $table.add($_)
-    }
-    end {
-        if ($table.count -eq 1) { # singleton detected
-            $newheader = if ($header -and $header.count -gt 1) {$header} else {'property','value'}
-            $_.psobject.properties.foreach{
-                [pscustomobject]@{
-                    $newheader[0] = $_.name
-                    $newheader[1] = $_.value
-                }
-            }
-            return
-        }
-        $rows = $table[0].psobject.properties.name # row identities
-        $colcount = $table.count
-        if ($include) {$colcount++}
-        $newheader = if ($header) {$header} 
-        else {for ($i=0; $i -lt $colcount; $i++) {"$prefix$i"}}
-        $start = if ($include) {1} else {0}
-        # Build new object
-        $objwidth = $newheader.count
-        foreach ($p in $rows) {
-            $row = '' | Select-Object $newheader
-            if ($include) {$row.($newheader[0]) = $p}
-            $i = $start
-            # Import data
-            foreach ($value in $table.$p) {
-                if ($i -ge $objwidth) {break}
-                $row.($newheader[$i++]) = $value
-            }
-            $row
-        }
-    }
-} # END transpose-object
-
-function transpose ([string[]]$header) {
-# for singletons only
-    process {
-        $newheader = if ($header -and $header.count -gt 1) {$header} else {'property','value'}
-        $_.psobject.properties.foreach{
-            [pscustomobject]@{
-                $newheader[0] = $_.name
-                $newheader[1] = $_.value
-            }
-        }
-    }
-} # END transpose
-
 function get-dayoff {
 # Russian dayoff json database importer
 # по этим базам странные данные: выходные и праздники - одно и то же; поэтому нужно фильтровать выходные (сб и вс)
@@ -125,3 +70,58 @@ function find-culture ([string]$culture) {
         }
     }}
 } # END find-culture
+
+function transpose-object ([string[]]$header, [switch]$include, [string]$prefix='col') {
+# $include - includes original header in the output object as the first column
+    begin {
+        $table = [System.Collections.Generic.List[object]]::new()
+        if (-not $prefix) {$prefix = 'col'}
+    }
+    process {
+        $table.add($_)
+    }
+    end {
+        if ($table.count -eq 1) { # singleton detected
+            $newheader = if ($header -and $header.count -gt 1) {$header} else {'property','value'}
+            $_.psobject.properties.foreach{
+                [pscustomobject]@{
+                    $newheader[0] = $_.name
+                    $newheader[1] = $_.value
+                }
+            }
+            return
+        }
+        $rows = $table[0].psobject.properties.name # row identities
+        $colcount = $table.count
+        if ($include) {$colcount++}
+        $newheader = if ($header) {$header} 
+        else {for ($i=0; $i -lt $colcount; $i++) {"$prefix$i"}}
+        $start = if ($include) {1} else {0}
+        # Build new object
+        $objwidth = $newheader.count
+        foreach ($p in $rows) {
+            $row = '' | Select-Object $newheader
+            if ($include) {$row.($newheader[0]) = $p}
+            $i = $start
+            # Import data
+            foreach ($value in $table.$p) {
+                if ($i -ge $objwidth) {break}
+                $row.($newheader[$i++]) = $value
+            }
+            $row
+        }
+    }
+} # END transpose-object
+
+function transpose ([string[]]$header) {
+# for singletons only
+    process {
+        $newheader = if ($header -and $header.count -gt 1) {$header} else {'property','value'}
+        $_.psobject.properties.foreach{
+            [pscustomobject]@{
+                $newheader[0] = $_.name
+                $newheader[1] = $_.value
+            }
+        }
+    }
+} # END transpose
