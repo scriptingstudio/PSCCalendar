406c261284f63c98424c486509d277b8794aea11
Matthew Gray <smotienko@gmail.com>
2022-03-25 09:01:04 +0300

    Update functions.ps1

--- functions.ps1
+++ functions.ps1
@@ -60,19 +60,6 @@ function get-dayoff {
     $DaysFoundHash            
 } # END get-dayoff
 
-function find-culture ([string]$culture) {
-    if (-not $culture) {$culture = '.*'}
-    [cultureinfo]::GetCultures('allCultures').where{$_.Name -match "$culture"} | . { process {
-        $dtf = [cultureinfo]::new($_).DateTimeFormat
-        [pscustomobject]@{
-            Culture  = $_.DisplayName
-            Id       = $_.Name
-            FDW      = $dtf.FirstDayOfWeek
-            Calendar = $dtf.Calendar.tostring().split('.')[2].replace('Calendar','')
-        }
-    }}
-} # END find-culture
-
 function transpose-object ([string[]]$header, [switch]$include, [string]$prefix='col') {
 # $include - includes original header in the output object as the first column
     begin {
