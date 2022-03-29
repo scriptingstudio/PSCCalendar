4fc58d30356c43a3a529fdbb9f0d15bff94a94d9
Matthew Gray <smotienko@gmail.com>
2022-03-29 18:21:52 +0300

    Collector v2.0

--- collector.ps1
+++ collector.ps1
@@ -1,10 +1,13 @@
+# Collector v2.0
+
 function get-calendarMonth {
 # Calendar data collector
 # Output: binary (raw) calendar and month markers
 # Output data go down the pipeline to the formatter tier
     param (
         [ValidateNotNullOrEmpty()]
-        [alias('month')][datetime]$start = (Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0),
+        [alias('month')][datetime]$start = [datetime]::new(([datetime]::now.year),([datetime]::now.month),1,0,0,0), #(Get-Date -Day 1 -Hour 0 -Minute 0 -Second 0) - slower
+
         [ValidateNotNullOrEmpty()]
         [System.DayOfWeek]$firstDay = 'Monday' # Mon - most used day among cultures
     )
@@ -24,7 +27,7 @@ function get-calendarMonth {
     $days    = 'D1','D2','D3','D4','D5','D6','D7' 
 
     # Adjust for the beginning of the calendar and the month
-    # calendar is a table of 7xN, N - amount of weeks
+    # calendar is a table of 7xN, N - number of weeks
     if ($start.day -ne 1) {$start = [datetime]::new($yr,$mo,1)} # validate $start
     $currentDay = $start # the beginning of the month
     while ($currentDay.DayOfWeek.value__ -ne $fd) {
@@ -44,9 +47,9 @@ function get-calendarMonth {
         $week
     }
     @{ # output protocol/interface
-        Calendar = $month
-        Month    = $mo
+        Calendar = $month # binary calendar
+        Month    = $mo    # month number
         Year     = $yr
-        FirstDay = $fd
+        FirstDay = $fd    # index in day name series
     }
 } # END get-calendarMonth
